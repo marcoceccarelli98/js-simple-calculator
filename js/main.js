d9da3b9181ee1f7f3faf10c2e12f5dbf2777ec5e
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-10 17:35:41 +0200

    [UPD] Optimized script

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@
 
 //Screen
 const screen = document.querySelector('.screen-num');
+
+//Pads
+const numPad = document.querySelector('.col-numpad');
+const signPad = document.querySelector('.col-signpad');
+
+//Buttons
 const numbersBtn = document.querySelectorAll('.numbers');
 const signsBtn = document.querySelectorAll('.symbols');
 const equalSign = document.getElementById('equal');
@@ -16,6 +22,7 @@ const cancelButton = document.getElementById('cancel');
 // ----
 
 let tempNum = '';
+let tempFullNum = '';
 let fullNum = '';
 let tempSign = '';
 let memFirstNum = 0;
@@ -23,77 +30,21 @@ let result = 0;
 
 // --- NUMBERS ---
 
-for (let i = 0; i < numbersBtn.length; i++) {
-    //add click event listener to all numbers
-    numbersBtn[i].addEventListener("click", function () {
-        //get number from innerHTML
-        tempNum = numbersBtn[i].innerHTML;
-        //concatenate number to get multiple digit numbers
-        fullNum += tempNum;
-        //add number to screen
-        screen.innerHTML = fullNum;
-    });
-}
+numPad.addEventListener('click', addNumbers);
 
 // --- SIGNS ---
 
-for (let i = 0; i < signsBtn.length; i++) {
-    //add click event listener to all signs
-    signsBtn[i].addEventListener("click", function () {
-
-        switch (signsBtn[i].id) {
-            case 'plus':
-                tempSign = 'plus';
-                break;
-            case 'minus':
-                tempSign = 'minus';
-                break;
-            case 'multi':
-                tempSign = 'multi';
-                break;
-            case 'divide':
-                tempSign = 'divide';
-                break;
-            case 'equal':
-                tempSign = 'equal';
-                break;
-        }
-
-        //save previus num to first num
-        memFirstNum = fullNum;
-        //reset temp number
-        fullNum = '';
-        //reset screen
-        screen.innerHTML = '';
-    });
-}
+signPad.addEventListener('click', addSign);
 
 // --- EQUAL ---
 
-//choice the operation to do
 equalSign.addEventListener("click", function () {
-    switch (tempSign) {
-        case 'plus':
-            result = sum(memFirstNum, fullNum);
-            break;
-        case 'minus':
-            result = min(memFirstNum, fullNum);
-            break;
-        case 'multi':
-            result = mul(memFirstNum, fullNum);
-            break;
-        case 'divide':
-            result = div(memFirstNum, fullNum);
-            break;
-    }
 
-    //check if user trying to divide a number by 0
-    if (fullNum == 0 && tempSign == 'divide') {
-        screen.innerHTML = 'Err';
-    } else {
-        //print result on screen
-        screen.innerHTML = result;
-    }
+    //do some operations
+    result = doMath(memFirstNum, fullNum, tempSign);
+    //check if user is trying to divide a number by 0
+    checkDivZero();
+
     //reset sign
     tempSign = '';
     //set temp num as result to get ready for another count
@@ -102,22 +53,87 @@ equalSign.addEventListener("click", function () {
 
 // --- CANCEL ---
 
-cancelButton.addEventListener("click", function () {
+cancelButton.addEventListener("click", reset);
+
+// --------
+// END MAIN
+// --------
+
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+
+// ---------
+// FUNCTIONS
+// ---------
+
+function addNumbers(e) {
+    if (e.target.classList.contains('numbers')) {
+        //get number from innerText
+        tempNum = e.target.innerText;
+        //concatenate number to get multiple digit numbers
+        fullNum += tempNum;
+    }
+
+    fullNum = removeInitialZero(fullNum);
+
+    //add number to screen
+    screen.innerText = fullNum;
+}
+
+function addSign(e) {
+    if (e.target.classList.contains('symbols')) {
+        //assign to temp sign the data-sign
+        tempSign = e.target.dataset.sign;
+        //memorize first number
+        memFirstNum = fullNum;
+        //reset temp number
+        fullNum = '';
+        //reset screen
+        screen.innerText = '';
+    }
+}
+
+function doMath(num1, num2, sign) {
+    switch (sign) {
+        case '+':
+            return sum(num1, num2);
+        case '-':
+            return min(num1, num2);
+        case '*':
+            return mul(num1, num2);
+        case '/':
+            return div(num1, num2);
+    }
+}
+
+function reset() {
     tempSign = '';
     tempNum = '';
     fullNum = '';
     memFirstNum = 0;
     result = 0;
-    screen.innerHTML = '';
-});
+    screen.innerText = '';
+}
 
 
+function removeInitialZero(string) {
+    // Check if string is empty or doesn't start with 0
+    if (string[0] === '0') {
+        string = (string.substring(1, string.length));
+    }
+    return string;
+}
 
-
-
-// ---------
-// FUNCTIONS
-// ---------
+// OPERATION
+function checkDivZero() {
+    //check if user trying to divide a number by 0
+    if (fullNum == 0 && tempSign == '/') {
+        screen.innerText = 'Err';
+    } else {
+        //print result on screen
+        screen.innerText = result;
+    }
+}
 
 function sum(num1, num2) {
     return Number(num1) + Number(num2);
